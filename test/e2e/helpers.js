f55944046bedbe56daaec4551c3d320ff3662a58
Javier <javier.vera@consensys.net>
2024-12-02 16:40:43 +0100

    feat: added mocks for solana snap on flask tests

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -944,6 +944,25 @@ async function openMenuSafe(driver) {
   }
 }
 
+async function enableExperimentalMode(driver) {
+  // Open settings menu button
+  const accountOptionsMenuSelector =
+    '[data-testid="account-options-menu-button"]';
+  await driver.waitForSelector(accountOptionsMenuSelector);
+  await driver.clickElement(accountOptionsMenuSelector);
+
+  // Click settings from dropdown menu
+  const globalMenuSettingsSelector = '[data-testid="global-menu-settings"]';
+  await driver.waitForSelector(globalMenuSettingsSelector);
+  await driver.clickElement(globalMenuSettingsSelector);
+  // Click Experimental tab
+  const experimentalTabRawLocator = {
+    text: 'Experimental',
+    tag: 'div',
+  };
+  await driver.clickElement(experimentalTabRawLocator);
+}
+
 const sentryRegEx = /^https:\/\/sentry\.io\/api\/\d+\/envelope/gu;
 
 module.exports = {
@@ -1000,4 +1019,5 @@ module.exports = {
   openMenuSafe,
   sentryRegEx,
   createWebSocketConnection,
+  enableExperimentalMode,
 };
